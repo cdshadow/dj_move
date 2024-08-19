03464a79b6caac90a6e009fc4ce2936abe131ffc
cdshadow <153569166+cdshadow@users.noreply.github.com>
2024-08-19 16:05:21 +0900

    Update dj_pop.py

--- dj_pop.py
+++ dj_pop.py
@@ -9,11 +9,11 @@ file_path = 'https://raw.githubusercontent.com/cdshadow/dj_move/main/data.csv'
 @st.cache_data
 def load_data(file_path):
     data = pd.read_csv(file_path, encoding='cp949')
+    # 년도를 문자열로 변환
+    data['년도'] = data['년도'].astype(str)
     return data
 
 data = load_data(file_path)
-
-# 2001년~2023년 대전시 순이동 데이터에 대한 꺾은선 그래프
 
 # Plotly를 이용한 꺾은선 그래프
 fig = px.line(data, x='년도', y='순이동 인구수', title='2001년~2023년 대전시 순이동 변화',
@@ -21,6 +21,9 @@ fig = px.line(data, x='년도', y='순이동 인구수', title='2001년~2023년 
 
 # x축의 모든 연도를 표시하도록 수정
 fig.update_xaxes(tickmode='linear', tick0=data['년도'].min(), dtick=1)
+
+# y축의 간격을 5,000 단위로 설정
+fig.update_yaxes(tick0=0, dtick=5000)
 
 # 그래프 커스터마이징 (선택 사항)
 fig.update_traces(line=dict(width=2))
