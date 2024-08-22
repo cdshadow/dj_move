9af5d9f4bc768bc4d8b56f2974593848269f7bdf
cdshadow <153569166+cdshadow@users.noreply.github.com>
2024-08-22 21:36:57 +0900

    Update dj_pop.py

--- dj_pop.py
+++ dj_pop.py
@@ -30,7 +30,7 @@ with st.sidebar:
     side_option = st.multiselect(
         label='지역 선택',
         options=['강원특별자치도', '경기도', '경상남도', '경상북도', '광주광역시', '대구광역시', '대전광역시', '부산광역시', '서울특별시', '세종특별자치시', '울산광역시', '인천광역시', '전라남도', '전북특별자치도', '제주특별자치도', '충청남도', '충청북도'],
-        placeholder='Select regions to filter'
+        placeholder='지역을 선택하세요'
     )
 
 with tab1:
